c87a54b81c3bc01680a31108604e8e4a6a11bd38
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2020-09-15 12:19:59 -0300

    update: atualização para a aula 230

---
+++ lib/views/orders_screen.dart
@@ -0,0 +1,23 @@
+//import 'dart:js';
+import 'package:cod3r_shop/widgets/app_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../widgets/app_drawer.dart';
+import '../providers/orders.dart';
+
+class OrdersScreens extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    final Orders orders = Provider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meus Pedidos'),
+      ),
+      drawer: AppDrawer(),
+      body: ListView.builder(
+        itemCount: orders.itemsCount,
+        itemBuilder: (ctx,i) => Text(orders.items[i].total.toString()),),
+    );
+  }
+}
